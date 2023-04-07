a8489bfda108613f877c1ec380d60325b4d189d7
kiranthedevelopr <kirannasim05@gmail.com>
2023-04-07 23:36:58 +0500

    created app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//*********************Chapter #1 Javascript******************************
+
+
+//Quesion number 1 //
+// alert("Hellow User! Welcome to the Website");
+
+
+//Qustion number 2
+// alert("Error! please enter a valid password.");
+
+//Qustion number 3
+// alert("Welcome to Js Land..  \nHappy Coding!");
+
+
+// alert("Welcome to Js Land..");
+// alert("Happy Coding!");
+
+// alert("Hello..I can run Js throuh my web browser,s console");
+
+
+
