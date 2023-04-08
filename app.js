69b533ad722815cf50719149d132010bc81a8788
kiranthedevelopr <kirannasim05@gmail.com>
2023-04-08 16:34:30 +0500

    Update app.js
    included a question numbers

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //*********************Chapter #1 Javascript******************************
 
 
-//Quesion number 1 //
+//********Quesion number 1********* //
 // alert("Hellow User! Welcome to the Website");
 
 
-//Qustion number 2
+//*******Qustion number 28*******//
 // alert("Error! please enter a valid password.");
 
-//Qustion number 3
+//Qustion number 3//
 // alert("Welcome to Js Land..  \nHappy Coding!");
 
-
+//*******Question number 4*******//
 // alert("Welcome to Js Land..");
 // alert("Happy Coding!");
 
+//******Question number 5********//
 // alert("Hello..I can run Js throuh my web browser,s console");
+
+
+//******Assighment Complted*******//
 
 
 
